a99ae0c34603159493e7266179ecf6007df1dad8
Shreyansh Sahare <shreyansh.sahare@fylehq.com>
2024-02-12 17:02:40 +0530

    Added support for release branch tag creation (#1)
    * Added support for release branch
    
    * updated name
    
    * updated name

--- build.sh
+++ build.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
+branch_name=$(git branch --show-current)
+if [ $branch_name == 'master' ]; then
+    NEW_TAG="v$(git rev-parse --short HEAD)"
+else
+    git fetch --tags
+    last_release_version=$(git tag --sort=-version:refname | grep ${branch_name} | head -n 1)
+
+    # if no release tag present, it means branch_name is newly created, so create first version
+    if [[ "$last_release_version" == "" ]]; then
+        NEW_TAG="${branch_name}_0"
+    else
+        current_patch_identifier=$(echo $last_release_version | awk -F _ '{print $NF}')
+        new_patch_identifier=$((current_patch_identifier + 1))
+        NEW_TAG="${branch_name}_${new_patch_identifier}"
+    fi
+
+    git config --global user.email "integrations@fylehq.com"
+    git config --global user.name "GitHub Actions"
+    git tag -a "${NEW_TAG}" -m "automatically tagged ${NEW_TAG}"
+    git push origin "${NEW_TAG}"
+
+fi
+echo "new_tag=$NEW_TAG" >> $GITHUB_OUTPUT
+
 docker login --username $DOCKERHUB_USERNAME --password $DOCKERHUB_PASSWORD
-export NEW_TAG="v$(git rev-parse --short HEAD)";
 docker build -t $DOCKERHUB_USERNAME/$IMAGE_NAME:$NEW_TAG .
 echo "Pushing Docker Image to Docker Hub";
 docker push $DOCKERHUB_USERNAME/$IMAGE_NAME:$NEW_TAG
